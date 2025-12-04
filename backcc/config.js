75390fb81f4fa5ef0e0207eb4516b445084ed839
JawadAhmadCS <mjawadahmad41@gmail.com>
2025-12-04 06:51:31 +0500

    ff

---
+++ backcc/config.js
@@ -0,0 +1,8 @@
+// Edit this to point to your deployed backend chat endpoint.
+// Example: http://localhost:3001/api/chat
+// If left blank or null, the UI will use mock responses for testing.
+
+const CONFIG = {
+  API_URL: "https://jawadahmadcs.pythonanywhere.com/api/ask", // <- default backend endpoint
+  MOCK_MODE: false
+};
